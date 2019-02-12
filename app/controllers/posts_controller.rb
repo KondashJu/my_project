70301b5f0056fbcd2770502659b280b8882d9207
Kondash Julia <juliakondash@gmail.com>
2019-02-13 00:22:03 +0200

    add images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :text, :date)
+      params.require(:post).permit(:title, :text, :date, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190212182608) do
+ActiveRecord::Schema.define(version: 20190212213122) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20190212182608) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
